18c33fdc2cb10c40fbc0e1163e4076af35137781
Gabriel <gpsnts@protonmail.com>
2022-08-04 15:23:45 -0300

    Initial bump

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
-#include <iostream>
-#include <vector>
 #include <string>
 
 #include "headers/gl_utils.hpp"
 
 int main(int argc, char** argv)
 {
-	GlUtils::debug_print<std::string>("ASD");
+	GlUtils::debug_print<std::string>("DEBUG MODE!");
 }
